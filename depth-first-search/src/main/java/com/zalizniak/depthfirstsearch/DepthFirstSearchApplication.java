47e1e5eed3efae249f8425cab72af07a9e2bda1b
aillusions <aillusions@gmail.com>
2019-02-17 14:25:30 +0200

    depth-first-search - wip

---
+++ depth-first-search/src/main/java/com/zalizniak/depthfirstsearch/DepthFirstSearchApplication.java
@@ -0,0 +1,13 @@
+package com.zalizniak.depthfirstsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepthFirstSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepthFirstSearchApplication.class, args);
+	}
+
+}
