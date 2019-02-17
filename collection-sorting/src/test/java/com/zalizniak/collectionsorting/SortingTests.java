8ef2bd2048c160214313123d872e90cdabf8b6fb
aillusions <aillusions@gmail.com>
2019-02-17 16:27:22 +0200

    collection-sorting - Quicksort

--- collection-sorting/src/test/java/com/zalizniak/collectionsorting/SortingTests.java
+++ collection-sorting/src/test/java/com/zalizniak/collectionsorting/SortingTests.java
@@ -30,4 +30,12 @@ public class SortingTests {
         System.out.println("Sorted array: " + Arrays.toString(arr));
         Assert.assertEquals("[11, 12, 22, 25, 34, 64, 90]", Arrays.toString(arr));
     }
+
+    @Test
+    public void quickSort() {
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        Quicksort.quickSort(arr);
+        System.out.println("Sorted array: " + Arrays.toString(arr));
+        Assert.assertEquals("[11, 12, 22, 25, 34, 64, 90]", Arrays.toString(arr));
+    }
 }
