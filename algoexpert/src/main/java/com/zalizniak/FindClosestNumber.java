81ae257c1dfecf73a2c4133a5812fc2c9ed7b221
alex <alex@platin.io>
2019-08-03 17:56:32 +0300

    find closest number

--- algoexpert/src/main/java/com/zalizniak/FindClosestNumber.java
+++ algoexpert/src/main/java/com/zalizniak/FindClosestNumber.java
@@ -3,6 +3,19 @@ package com.zalizniak;
 public class FindClosestNumber {
 
     public static int getClosest(int given, int[] array) {
-        return array[0];
+
+        int minDiff = Integer.MAX_VALUE;
+        int rv = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            int thisValue = array[i];
+            int diff = Math.abs(given - thisValue);
+            if (minDiff > diff) {
+                minDiff = diff;
+                rv = thisValue;
+            }
+        }
+
+        return rv;
     }
 }
--- algoexpert/src/test/java/com/zalizniak/FindClosestNumberTest.java
+++ algoexpert/src/test/java/com/zalizniak/FindClosestNumberTest.java
@@ -7,6 +7,9 @@ public class FindClosestNumberTest {
 
     @Test
     public void shouldFindClosest() {
+        TestCase.assertEquals(10, FindClosestNumber.getClosest(9, new int[]{2, 10, 20}));
         TestCase.assertEquals(82, FindClosestNumber.getClosest(80, new int[]{2, 42, 82, 122, 162, 202, 242, 282, 322, 362}));
+        TestCase.assertEquals(400, FindClosestNumber.getClosest(400, new int[]{100, 200, 400, 800, 1600, 3200, 6400, 128000}));
+        TestCase.assertEquals(128000, FindClosestNumber.getClosest(128000, new int[]{100, 200, 400, 800, 1600, 3200, 6400, 128000}));
     }
 }
