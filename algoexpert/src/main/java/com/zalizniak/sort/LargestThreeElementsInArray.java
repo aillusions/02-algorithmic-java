40264d3238572704bcfa9193f5ff33288e689136
alex <alex@platin.io>
2019-08-03 15:16:04 +0300

    find the biggest three numbers in an array

--- algoexpert/src/main/java/com/zalizniak/sort/LargestThreeElementsInArray.java
+++ algoexpert/src/main/java/com/zalizniak/sort/LargestThreeElementsInArray.java
@@ -1,10 +1,18 @@
 package com.zalizniak.sort;
 
+import java.util.LinkedList;
+
 public class LargestThreeElementsInArray {
 
     public static int[] findBiggestThreeNumbers(int[] in) {
-        int[] rv = new int[3];
 
-        return in;
+        LinkedList<Integer> intermediate = new LinkedList<>();
+        for (int value : in) {
+            intermediate.add(value);
+        }
+
+        intermediate.sort((o1, o2) -> -Integer.compare(o1, o2));
+
+        return intermediate.subList(0, 3).stream().mapToInt(e -> e).toArray();
     }
 }
--- algoexpert/src/test/java/com/zalizniak/sort/LargestThreeElementsInArrayTest.java
+++ algoexpert/src/test/java/com/zalizniak/sort/LargestThreeElementsInArrayTest.java
@@ -8,5 +8,9 @@ public class LargestThreeElementsInArrayTest {
     @Test
     public void shouldFindTreeLargest() {
         Assert.assertArrayEquals(new int[]{3, 2, 1}, LargestThreeElementsInArray.findBiggestThreeNumbers(new int[]{3, 2, 1}));
+
+        Assert.assertArrayEquals(new int[]{3, 2, 1}, LargestThreeElementsInArray.findBiggestThreeNumbers(new int[]{1, 2, 3}));
+
+        Assert.assertArrayEquals(new int[]{6, 5, 4}, LargestThreeElementsInArray.findBiggestThreeNumbers(new int[]{1, 2, 3, 4, 5, 6}));
     }
 }
