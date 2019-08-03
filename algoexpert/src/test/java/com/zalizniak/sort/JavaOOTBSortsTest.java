b4fb4b04bc85b6c152cbd9c6cb5f96442db15f11
alex <alex@platin.io>
2019-08-03 18:56:26 +0300

    java sort algorithms

--- algoexpert/src/test/java/com/zalizniak/sort/JavaOOTBSortsTest.java
+++ algoexpert/src/test/java/com/zalizniak/sort/JavaOOTBSortsTest.java
@@ -2,14 +2,21 @@ package com.zalizniak.sort;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class JavaOOTBSortsTest {
 
-    private static final Integer[] ARRAY = new Integer[]{1, 5, 7, 66, 88, 89, 123, 200, 255, 300000000, 1, 21, 2, 3, 22, 4, 5, 23, 6, 7, 24, 8, 9, 25, 200000000, 10, 11, 26, 12, 13, 27, 14, 15, 28, 16, 17, 29, 18, 30, 19, 100000000, 1, 5, 7, 66, 88, 89, 123, 200, 255};
+    private static final int SIZE = 1_000_000;
+    private static final Integer[] ARRAY = new Integer[SIZE];
+
+    static {
+        Random RAND = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            ARRAY[i] = RAND.nextInt();
+        }
+    }
+
     private static final List<Integer> LIST = Arrays.asList(ARRAY);
 
     private static final Comparator<Integer> INTEGER_COMPARATOR = (o1, o2) -> -Integer.compare(o1, o2);
@@ -20,7 +27,17 @@ public class JavaOOTBSortsTest {
     }
 
     @Test
+    public void shouldSortArrayParallel() {
+        Arrays.parallelSort(ARRAY, INTEGER_COMPARATOR);
+    }
+
+    @Test
     public void shouldSortList() {
-        Collections.sort(LIST, INTEGER_COMPARATOR);
+        LIST.sort(INTEGER_COMPARATOR);
+    }
+
+    @Test
+    public void shouldSortListParallel() {
+        LIST.parallelStream().sorted().collect(Collectors.toList());
     }
 }
