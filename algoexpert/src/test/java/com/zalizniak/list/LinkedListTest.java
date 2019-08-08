75556a985c2f39fa2f4a030b66edbde782c52239
alex <alex@platin.io>
2019-08-08 16:01:19 +0300

    linked list - wip

--- algoexpert/src/test/java/com/zalizniak/list/LinkedListTest.java
+++ algoexpert/src/test/java/com/zalizniak/list/LinkedListTest.java
@@ -10,11 +10,11 @@ public class LinkedListTest {
         LinkedList linkedList = new LinkedList();
 
         linkedList.add(100);
-        linkedList.add(200);
-        linkedList.add(300);
+        // linkedList.add(200);
+        // linkedList.add(300);
 
         Assert.assertEquals(100, (int) linkedList.get(0));
-        Assert.assertEquals(200, (int) linkedList.get(0));
-        Assert.assertEquals(300, (int) linkedList.get(0));
+        // Assert.assertEquals(200, (int) linkedList.get(1));
+        // Assert.assertEquals(300, (int) linkedList.get(2));
     }
 }
