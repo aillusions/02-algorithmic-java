d64f1d81ed43a5eea9c2c14846fab0fb5321655e
alex <alex@platin.io>
2019-08-08 17:26:53 +0300

    Sorting - bubble sort

--- algoexpert/src/main/java/com/zalizniak/sort/MineAttemptSort.java
+++ algoexpert/src/main/java/com/zalizniak/sort/MineAttemptSort.java
@@ -9,6 +9,14 @@ public class MineAttemptSort {
         return in;
     }
 
+    /**
+     * In Bubble Sort, n-1 comparisons will be done in the 1st pass, n-2 in 2nd pass, n-3 in 3rd pass and so on.
+     * So the total number of comparisons will be:
+     *
+     * (n-1) + (n-2) + (n-3) + ..... + 3 + 2 + 1
+     * Sum = n(n-1)/2
+     * i.e O(n2)
+     */
     public static Integer[] bubbleSort(Integer[] in) {
         for (int j = 0; j < in.length - 1; j++) {
             for (int i = 0; i < (in.length - j - 1); i++) {
