187faf746be00f6503235d90b6b11a90bc4b0f46
alex <alex@platin.io>
2019-08-09 18:22:03 +0300

    BST validation

--- algoexpert/src/main/java/com/zalizniak/medium/tree/bst/BSTValidator.java
+++ algoexpert/src/main/java/com/zalizniak/medium/tree/bst/BSTValidator.java
@@ -1,10 +1,35 @@
 package com.zalizniak.medium.tree.bst;
 
+import java.util.Stack;
+
 public class BSTValidator {
 
     public static boolean isValid(BinaryTreeNode rootNode) {
 
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        stack.push(rootNode);
 
-        return false;
+        while (!stack.isEmpty()) {
+            BinaryTreeNode itNode = stack.pop();
+
+            if (itNode.left != null) {
+                if (itNode.data < itNode.left.data) {
+                    return false;
+                }
+                stack.push(itNode.left);
+            }
+
+            if (itNode.right != null) {
+                if (itNode.data > itNode.right.data) {
+                    return false;
+                }
+                stack.push(itNode.right);
+            }
+
+            System.out.println("Visited: " + itNode);
+
+        }
+
+        return true;
     }
 }
