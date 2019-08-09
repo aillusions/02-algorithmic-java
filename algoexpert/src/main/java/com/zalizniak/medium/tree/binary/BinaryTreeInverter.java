2d99b48ef29ea5baf8e54701f16cd3a0778b03ad
alex <alex@platin.io>
2019-08-09 19:02:30 +0300

    Binary tree invert

--- algoexpert/src/main/java/com/zalizniak/medium/tree/binary/BinaryTreeInverter.java
+++ algoexpert/src/main/java/com/zalizniak/medium/tree/binary/BinaryTreeInverter.java
@@ -1,8 +1,27 @@
 package com.zalizniak.medium.tree.binary;
 
+import java.util.Stack;
+
 public class BinaryTreeInverter {
 
     public static void invertTree(BinaryTreeNode root) {
 
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            BinaryTreeNode node = stack.pop();
+
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+
+            BinaryTreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
     }
 }
