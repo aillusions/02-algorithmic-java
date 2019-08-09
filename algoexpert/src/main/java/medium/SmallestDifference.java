2eae53dc51feeccb0b1910a7f37082d740db4bd7
alex <alex@platin.io>
2019-08-09 11:20:41 +0300

    minimum distance

---
+++ algoexpert/src/main/java/medium/SmallestDifference.java
@@ -0,0 +1,22 @@
+package medium;
+
+public class SmallestDifference {
+
+    public static int getSmallestDiff(int[] in) {
+
+        int rv = Integer.MAX_VALUE;
+
+        for (int i = 0; i < in.length; i++) {
+            for (int j = 0; j < in.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                int diff = Math.abs(in[i] - in[j]);
+                if (rv > diff) {
+                    rv = diff;
+                }
+            }
+        }
+        return rv;
+    }
+}
---
+++ algoexpert/src/test/java/com/zalizniak/medium/SmallestDifferenceTest.java
@@ -0,0 +1,14 @@
+package com.zalizniak.medium;
+
+import medium.SmallestDifference;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SmallestDifferenceTest {
+
+    @Test
+    public void shouldFindSmallestDifference() {
+        Assert.assertEquals(1, SmallestDifference.getSmallestDiff(new int[]{1, 2}));
+        Assert.assertEquals(2, SmallestDifference.getSmallestDiff(new int[]{5, 10, 20, 100, 3}));
+    }
+}
