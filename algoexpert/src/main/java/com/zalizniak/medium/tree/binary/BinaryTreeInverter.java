b9402a214976b6b708d1a16de6953649c9251590
alex <alex@platin.io>
2019-08-09 18:52:31 +0300

    Binary tree invert - wip

---
+++ algoexpert/src/main/java/com/zalizniak/medium/tree/binary/BinaryTreeInverter.java
@@ -0,0 +1,8 @@
+package com.zalizniak.medium.tree.binary;
+
+public class BinaryTreeInverter {
+
+    public void invertTree(BinaryTreeNode root) {
+
+    }
+}
---
+++ algoexpert/src/test/java/com/zalizniak/medium/tree/binary/BinaryTreeInverterTest.java
@@ -0,0 +1,27 @@
+package com.zalizniak.medium.tree.binary;
+
+import org.junit.Test;
+
+public class BinaryTreeInverterTest {
+
+    @Test
+    public void shouldInvertBinaryTree() {
+
+        BinaryTreeNode n0 = new BinaryTreeNode(0);
+        BinaryTreeNode n1 = new BinaryTreeNode(1);
+        BinaryTreeNode n2 = new BinaryTreeNode(2);
+        BinaryTreeNode n3 = new BinaryTreeNode(3);
+        BinaryTreeNode n4 = new BinaryTreeNode(4);
+        BinaryTreeNode n5 = new BinaryTreeNode(5);
+
+        n1.left = n0;
+
+        n2.left = n1;
+        n2.right = n3;
+
+        n3.left = n4;
+        n3.right = n5;
+
+        BinaryTreePrinter.printNode(n2);
+    }
+}
