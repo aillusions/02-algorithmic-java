873d099c02d3d94ad36250a18e2ee6803315aeff
alex <alex@platin.io>
2019-08-09 10:51:14 +0300

    Find a triplet that sum to a given value - wip

--- algoexpert/src/main/java/medium/ThreeNumberSum.java
+++ algoexpert/src/main/java/medium/ThreeNumberSum.java
@@ -1,23 +1,40 @@
 package medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ThreeNumberSum {
 
     public static Integer[] findThreeNumberSum(Integer[] in, int sum) {
 
+        final Set<Integer> checking = new HashSet<>();
+
         for (int i = 0; i < in.length; i++) {
+            if (checking.contains(i)) {
+                continue;
+            } else {
+                checking.add(i);
+            }
             for (int j = 0; j < in.length; j++) {
-                if (j == i) {
+                if (checking.contains(j)) {
                     continue;
+                } else {
+                    checking.add(j);
                 }
                 for (int k = 0; k < in.length; k++) {
-                    if (k == i || k == j) {
+                    if (checking.contains(k)) {
                         continue;
+                    } else {
+                        checking.add(k);
                     }
                     if (in[i] + in[j] + in[k] == sum) {
                         return new Integer[]{in[i], in[j], in[k]};
                     }
+                    checking.remove(k);
                 }
+                checking.remove(j);
             }
+            checking.remove(i);
         }
 
         return null;
--- algoexpert/src/test/java/com/zalizniak/medium/ThreeNumberSumTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/ThreeNumberSumTest.java
@@ -3,12 +3,14 @@ package com.zalizniak.medium;
 import medium.ThreeNumberSum;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.collection.IsArrayContainingInAnyOrder;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ThreeNumberSumTest {
 
     @Test
     public void testThreeNumberSum() {
+        Assert.assertNull(ThreeNumberSum.findThreeNumberSum(new Integer[]{1, 2, 3, 4}, 10));
         MatcherAssert.assertThat(ThreeNumberSum.findThreeNumberSum(new Integer[]{1, 2, 3, 4}, 6), IsArrayContainingInAnyOrder.arrayContainingInAnyOrder(1, 2, 3));
         MatcherAssert.assertThat(ThreeNumberSum.findThreeNumberSum(new Integer[]{12, 3, 4, 1, 6, 9}, 24), IsArrayContainingInAnyOrder.arrayContainingInAnyOrder(12, 3, 9));
     }
