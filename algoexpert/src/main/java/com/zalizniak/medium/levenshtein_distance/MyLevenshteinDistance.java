652d4085bd8ab187d624ea5831c36f5f5f932e14
alex <alex@platin.io>
2019-08-11 12:24:11 +0300

    Levenshtein distance- wip

--- algoexpert/src/main/java/com/zalizniak/medium/levenshtein_distance/MyLevenshteinDistance.java
+++ algoexpert/src/main/java/com/zalizniak/medium/levenshtein_distance/MyLevenshteinDistance.java
@@ -2,19 +2,36 @@ package com.zalizniak.medium.levenshtein_distance;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyLevenshteinDistance implements LevenshteinDistance {
 
     @Override
     public int getDistance(String a, String b) {
-        return getDistance(new StringBuilder(a), new StringBuilder(b), Arrays.asList("i", "d", "s"));
+
+        return getDistance(a.chars().mapToObj(i -> (char) i).collect(Collectors.toList()),
+                b.chars().mapToObj(i -> (char) i).collect(Collectors.toList()),
+                Arrays.asList("i", "d", "s"));
     }
 
-    public int getDistance(StringBuilder basis, StringBuilder modifier, List<String> operations) {
-        if (basis.equals(modifier)) {
+    public int getDistance(List<Character> a, List<Character> b, List<String> operations) {
+        //if (a.toString().equals(b.toString())) {
+        if (a.isEmpty() && b.isEmpty()) {
             return 0;
         }
 
-        return 1;
+        if (a.get(0) == b.get(0)) {
+            return getDistance(a.subList(1, a.size()), b.subList(1, b.size()), operations);
+        }
+
+        //int minDelta = 0;
+        //for (String operation : operations) {
+//
+        //}
+//
+        //return getDistance(a, b, operations);
+        return 0;
     }
+
+
 }
