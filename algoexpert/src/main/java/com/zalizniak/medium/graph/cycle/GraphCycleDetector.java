2a9b032cee462e5700d38bfd1efbc1459f6fef6f
alex <alex@platin.io>
2019-08-11 15:44:04 +0300

    Detect Cycle in a Directed Graph - wip

--- algoexpert/src/main/java/com/zalizniak/medium/graph/cycle/GraphCycleDetector.java
+++ algoexpert/src/main/java/com/zalizniak/medium/graph/cycle/GraphCycleDetector.java
@@ -2,20 +2,30 @@ package com.zalizniak.medium.graph.cycle;
 
 import com.zalizniak.medium.graph.Vertex;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 public class GraphCycleDetector {
 
     public boolean hasCycle(Vertex root) {
 
+        Set<Vertex> visited = new HashSet<>();
+
         Stack<Vertex> stack = new Stack<>();
         stack.add(root);
 
         while (!stack.isEmpty()) {
             Vertex vertex = stack.pop();
-            stack.addAll(vertex.vertices);
-
             System.out.println("Visited: " + vertex.label);
+
+            if (visited.contains(vertex)) {
+                return true;
+            }
+
+            visited.add(vertex);
+
+            stack.addAll(vertex.vertices);
         }
 
         return false;
--- algoexpert/src/test/java/com/zalizniak/medium/graph/cycle/GraphCycleDetectorTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/graph/cycle/GraphCycleDetectorTest.java
@@ -9,7 +9,39 @@ public class GraphCycleDetectorTest {
     private GraphCycleDetector graphCycleDetector = new GraphCycleDetector();
 
     @Test
-    public void test() {
+    public void testNoCycle() {
+
+        Vertex n0 = new Vertex(0);
+        Vertex n1 = new Vertex(1);
+        Vertex n2 = new Vertex(2);
+        Vertex n3 = new Vertex(3);
+        Vertex n4 = new Vertex(4);
+
+        n0.add(n1, n2);
+        n2.add(n3);
+        n3.add(n4);
+
+        Assert.assertFalse(graphCycleDetector.hasCycle(n0));
+    }
+    @Test
+    public void testCycle() {
+
+        Vertex n0 = new Vertex(0);
+        Vertex n1 = new Vertex(1);
+        Vertex n2 = new Vertex(2);
+        Vertex n3 = new Vertex(3);
+        Vertex n4 = new Vertex(4);
+
+        n0.add(n1, n2);
+        n2.add(n3);
+        n3.add(n4);
+        n4.add(n0);
+
+        Assert.assertTrue(graphCycleDetector.hasCycle(n0));
+    }
+
+    @Test
+    public void testCycle1() {
 
         Vertex n0 = new Vertex(0);
         Vertex n1 = new Vertex(1);
