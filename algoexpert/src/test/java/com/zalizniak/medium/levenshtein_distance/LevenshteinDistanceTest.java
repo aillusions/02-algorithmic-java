9936c480f4c5a3eb2c82ab1cb7c874584ba67171
alex <alex@platin.io>
2019-08-11 12:59:05 +0300

    Levenshtein distance

--- algoexpert/src/test/java/com/zalizniak/medium/levenshtein_distance/LevenshteinDistanceTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/levenshtein_distance/LevenshteinDistanceTest.java
@@ -14,5 +14,17 @@ public class LevenshteinDistanceTest {
         Assert.assertEquals(2, calc.getDistance("GILY", "GEELY"));
         Assert.assertEquals(3, calc.getDistance("HONDA", "HYUNDAI"));
         Assert.assertEquals(1, calc.getDistance("shot", "spot"));
+
+
+        // https://commons.apache.org/sandbox/commons-text/jacoco/org.apache.commons.text.similarity/LevenshteinDistance.java.html
+        Assert.assertEquals(0, calc.getDistance("", ""));
+        Assert.assertEquals(1, calc.getDistance("", "a"));
+        Assert.assertEquals(7, calc.getDistance("aaapppp", ""));
+        Assert.assertEquals(1, calc.getDistance("frog", "fog"));
+        Assert.assertEquals(3, calc.getDistance("fly", "ant"));
+        Assert.assertEquals(7, calc.getDistance("elephant", "hippo"));
+        Assert.assertEquals(7, calc.getDistance("hippo", "elephant"));
+        Assert.assertEquals(8, calc.getDistance("hippo", "zzzzzzzz"));
+        Assert.assertEquals(1, calc.getDistance("hello", "hallo"));
     }
 }
