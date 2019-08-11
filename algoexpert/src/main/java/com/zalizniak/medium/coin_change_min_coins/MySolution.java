02588abbd6c91dd7a0f1ec3efcf959a8b7bb7def
alex <alex@platin.io>
2019-08-11 11:21:53 +0300

    Minimum number of coins that make a given value

--- algoexpert/src/main/java/com/zalizniak/medium/coin_change_min_coins/MySolution.java
+++ algoexpert/src/main/java/com/zalizniak/medium/coin_change_min_coins/MySolution.java
@@ -6,7 +6,19 @@ public class MySolution implements ChangeMinCoins {
 
     @Override
     public int minCoins(int sum, List<Integer> coins) {
+        int rv = sum;
 
+        for (int i = 0; i < coins.size(); i++) {
+            int biggest = biggestCoins(sum, coins.subList(i, coins.size()));
+            if (biggest < rv) {
+                rv = biggest;
+            }
+        }
+
+        return rv;
+    }
+
+    public int biggestCoins(int sum, List<Integer> coins) {
         if (sum <= 0 || coins.isEmpty()) {
             return 0;
         }
