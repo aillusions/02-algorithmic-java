bd6146f60ee650cfc0e023475a1b6eb76e74d85f
alex <alex@platin.io>
2019-08-11 15:06:42 +0300

    Detect Cycle in a Directed Graph - wip

--- algoexpert/src/main/java/com/zalizniak/medium/graph/cycle/GraphCycleDetector.java
+++ algoexpert/src/main/java/com/zalizniak/medium/graph/cycle/GraphCycleDetector.java
@@ -2,9 +2,22 @@ package com.zalizniak.medium.graph.cycle;
 
 import com.zalizniak.medium.graph.Vertex;
 
+import java.util.Stack;
+
 public class GraphCycleDetector {
 
     public boolean hasCycle(Vertex root) {
+
+        Stack<Vertex> stack = new Stack<>();
+        stack.add(root);
+
+        while (!stack.isEmpty()) {
+            Vertex vertex = stack.pop();
+            stack.addAll(vertex.vertices);
+
+            System.out.println("Visited: " + vertex.label);
+        }
+
         return false;
     }
 }
