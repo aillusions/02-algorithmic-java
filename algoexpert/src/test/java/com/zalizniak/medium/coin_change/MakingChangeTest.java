eee8fa6fc7365d0726cbd75e8af29f2e57fb96bb
alex <alex@platin.io>
2019-08-11 09:52:57 +0300

    Integer partition - coin change problem

--- algoexpert/src/test/java/com/zalizniak/medium/coin_change/MakingChangeTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/coin_change/MakingChangeTest.java
@@ -35,4 +35,9 @@ public class MakingChangeTest {
         Assert.assertEquals(2, maker.numberOfWays(6, DENOMINATIONS)); // {5,1} {1,1,1,1,1,1}
         Assert.assertEquals(4, maker.numberOfWays(10, DENOMINATIONS)); // {10}, {5,5} {1,1,1,1,1,1,1,1,1,1} {5,1,1,1,1,1)
     }
+
+    @Test
+    public void largeAmount() {
+        Assert.assertEquals(1771, maker.numberOfWays(200, DENOMINATIONS));
+    }
 }
