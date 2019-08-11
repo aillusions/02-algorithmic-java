c704294ddf5d11e0312c1ce68cae44fa53ef4251
alex <alex@platin.io>
2019-08-11 09:47:49 +0300

    Integer partition - coin change problem

--- algoexpert/src/main/java/com/zalizniak/medium/coin_change/MySolution.java
+++ algoexpert/src/main/java/com/zalizniak/medium/coin_change/MySolution.java
@@ -4,43 +4,39 @@ import java.util.List;
 
 /**
  * https://www.ideserve.co.in/learn/coin-change-problem-number-of-ways-to-make-change
- *
+ * <p>
  * If we are make change of 50 using infinite number of coins of denominations {20,10,5,1} then
  * total number of ways to make change of 50 using denominations {20,10,5,1} = total number of ways to make change of 50 using 0 coins of 20 + total number of ways to make change of 50 using 1 coin of 20 + total number of ways to make change of 50 using 2 coins of 20
  * Now first term on the right hand side of above equation that is - total number of ways to make change of 50 using 0 coins of 20 can be restated as total number of ways to make change of 50 using denominations {10,5,1}
  * And second term that is total number of ways to make change of 50 using 1 coin of 20 = total number of ways to make change of 30 using denominations {10,5,1}
  * Similarly, total number of ways to make change of 50 using 2 coins of 20 = total number of ways to make change of 10 using denominations {10,5,1}.
- *
+ * <p>
  * Time Complexity is O(nm)
  * Space Complexity is O(nm)
- *
  */
 public class MySolution implements MakingChange {
 
     /**
      * Assuming ordered
-     *
-     *
      */
     @Override
     public int numberOfWays(int sum, List<Integer> coins) {
 
-        int thisSumResult = 0;
-        for (int i = 0; i < coins.size(); i++) {
-            int denom = coins.get(i);
-            int maxCoinsPerDenom = sum / denom;
-
-            for (int j = 0; j < sum; j++) {
-
-            }
+        if (sum == 0) {
+            return 1;
         }
 
-        if (coins.size() > 1) {
-            int preSumResult = numberOfWays(sum, coins.subList(1, coins.size()));
-            return thisSumResult + preSumResult;
-        } else {
-            return thisSumResult;
+        if (sum < 0 || coins.isEmpty()) {
+            return 0;
         }
 
+        final int denomination = coins.get(0);
+        final int sumRemainded = sum - denomination;
+        final List<Integer> denomRemainded = coins.subList(1, coins.size());
+
+        final int incl = numberOfWays(sumRemainded, coins);
+        final int excl = numberOfWays(sum, denomRemainded);
+
+        return incl + excl;
     }
 }
--- algoexpert/src/test/java/com/zalizniak/medium/coin_change/MakingChangeTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/coin_change/MakingChangeTest.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @RunWith(Parameterized.class)
 public class MakingChangeTest {
 
-    private static final List<Integer> DENOMINATIONS_NATURAL = Arrays.asList(20, 10, 5, 1);
-    //private static final List<Integer> DENOMINATIONS_MATH = Arrays.asList(3, 2, 1);
+    private static final List<Integer> DENOMINATIONS = Arrays.asList(20, 10, 5, 1);
 
     private MakingChange maker;
 
@@ -28,15 +29,10 @@ public class MakingChangeTest {
 
     @Test
     public void shouldFindSum() {
-        Assert.assertEquals(1, maker.numberOfWays(1, DENOMINATIONS_NATURAL)); // {1}
-        Assert.assertEquals(1, maker.numberOfWays(4, DENOMINATIONS_NATURAL)); // {1,1,1,1}
-        Assert.assertEquals(2, maker.numberOfWays(5, DENOMINATIONS_NATURAL)); // {5}, {1,1,1,1,1}
-        Assert.assertEquals(2, maker.numberOfWays(6, DENOMINATIONS_NATURAL)); // {5,1} {1,1,1,1,1,1}
-        Assert.assertEquals(3, maker.numberOfWays(10, DENOMINATIONS_NATURAL)); // {5,5} {1,1,1,1,1,1,1,1,1,1} {5,1,1,1,1,1)
+        Assert.assertEquals(1, maker.numberOfWays(1, DENOMINATIONS)); // {1}
+        Assert.assertEquals(1, maker.numberOfWays(4, DENOMINATIONS)); // {1,1,1,1}
+        Assert.assertEquals(2, maker.numberOfWays(5, DENOMINATIONS)); // {5}, {1,1,1,1,1}
+        Assert.assertEquals(2, maker.numberOfWays(6, DENOMINATIONS)); // {5,1} {1,1,1,1,1,1}
+        Assert.assertEquals(4, maker.numberOfWays(10, DENOMINATIONS)); // {10}, {5,5} {1,1,1,1,1,1,1,1,1,1} {5,1,1,1,1,1)
     }
-
-    //@Test
-    //public void shouldFindSum0() {
-    //    Assert.assertEquals(4, maker.numberOfWays(4, DENOMINATIONS_MATH));
-    //}
 }
