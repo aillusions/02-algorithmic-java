ada6b177fb07f56d7153062c5e936fd2f9e4db86
alex <alex@platin.io>
2019-08-11 13:14:04 +0300

    Levenshtein distance - iterative

--- algoexpert/src/test/java/com/zalizniak/medium/levenshtein_distance/LevenshteinDistanceTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/levenshtein_distance/LevenshteinDistanceTest.java
@@ -2,10 +2,28 @@ package com.zalizniak.medium.levenshtein_distance;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class LevenshteinDistanceTest {
 
-    private LevenshteinDistance calc = new MyLevenshteinDistance();
+    private LevenshteinDistance calc;
+
+    public LevenshteinDistanceTest(LevenshteinDistance calc) {
+        this.calc = calc;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> getParameters() {
+        return Arrays.asList(new Object[][]{
+                {new MyLevenshteinDistance()},
+                {new IterativeLevenshteinDistance()}
+        });
+    }
 
     @Test
     public void calcForLongSentence() {
