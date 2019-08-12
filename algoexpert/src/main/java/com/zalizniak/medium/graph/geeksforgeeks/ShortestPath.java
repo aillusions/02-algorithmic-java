2e35622257bed135a19cf76b21a31bd601526043
alex <alex@platin.io>
2019-08-12 12:04:10 +0300

    BFS - shortest path

--- algoexpert/src/main/java/com/zalizniak/medium/graph/geeksforgeeks/ShortestPath.java
+++ algoexpert/src/main/java/com/zalizniak/medium/graph/geeksforgeeks/ShortestPath.java
@@ -1,5 +1,8 @@
 package com.zalizniak.medium.graph.geeksforgeeks;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * see directed-unweighted-predefined-graph-of-5-nodes.png
  */
@@ -15,10 +18,31 @@ public class ShortestPath {
             /*5*/ {2}
     };
 
-    public int getShortestPath(int startNode, int endNode){
+    public int getShortestPath(int startNode, int endNode) {
+
+        int[] distances = new int[adjacencyLists.length];
+
+        boolean[] visited = new boolean[adjacencyLists.length];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(startNode);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.println(startNode + " -> " + endNode + ": visited " + node);
+            visited[node] = true;
+
+            for (int i = 0; i < adjacencyLists[node].length; i++) {
+                int adjacentToNode = adjacencyLists[node][i];
+                if (!visited[adjacentToNode]) {
+                    queue.add(adjacentToNode);
+                }
+            }
+        }
+
+        System.out.println();
+
         return 0;
-
-
     }
 
 }
