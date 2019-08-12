9c5e889bc4964dd977a1c847bc7dc939b6793402
alex <alex@platin.io>
2019-08-12 15:35:30 +0300

    River sizes

--- algoexpert/src/main/java/com/zalizniak/medium/river_sizes/RiverSizes.java
+++ algoexpert/src/main/java/com/zalizniak/medium/river_sizes/RiverSizes.java
@@ -1,6 +1,5 @@
 package com.zalizniak.medium.river_sizes;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
@@ -11,7 +10,8 @@ import java.util.Stack;
 public class RiverSizes {
 
     private final List<Integer>[] adjacentLists;
-    private final int[] nodeValues;
+    private final List<Integer> riverNodes = new LinkedList<>();
+
     private final int totalElementsOfMatrix;
     private final int matrixWidth;
     private final int matrixHeight;
@@ -21,7 +21,6 @@ public class RiverSizes {
         matrixWidth = matrix[0].length;
         totalElementsOfMatrix = matrixWidth * matrixHeight;
         adjacentLists = new List[totalElementsOfMatrix];
-        nodeValues = new int[totalElementsOfMatrix];
 
         for (int h = 0; h < matrix.length; h++) {
             for (int w = 0; w < matrix.length; w++) {
@@ -29,70 +28,73 @@ public class RiverSizes {
                 int currentElementIdx = (matrixWidth * h) + w;
                 List<Integer> adjacentList = new LinkedList<>();
                 adjacentLists[currentElementIdx] = adjacentList;
-                nodeValues[currentElementIdx] = matrix[h][w];
+                int nodeValue = matrix[h][w];
+                if (nodeValue == 0) {
+                    continue;
+                }
+
+                riverNodes.add(currentElementIdx);
 
                 if (h - 1 >= 0) {
-                    //if (matrix[h - 1][w] == 1) {
-                    adjacentList.add((matrixWidth * (h - 1)) + w);
-                    //}
+                    if (matrix[h - 1][w] == 1) {
+                        adjacentList.add((matrixWidth * (h - 1)) + w);
+                    }
                 }
 
                 if (h + 1 < matrixHeight) {
-                    //if (matrix[h + 1][w] == 1) {
-                    adjacentList.add((matrixWidth * (h + 1)) + w);
-                    //}
+                    if (matrix[h + 1][w] == 1) {
+                        adjacentList.add((matrixWidth * (h + 1)) + w);
+                    }
                 }
 
                 if (w - 1 >= 0) {
-                    //if (matrix[h][w - 1] == 1) {
-                    adjacentList.add((matrixWidth * h) + (w - 1));
-                    //}
+                    if (matrix[h][w - 1] == 1) {
+                        adjacentList.add((matrixWidth * h) + (w - 1));
+                    }
                 }
 
                 if (w + 1 < matrixWidth) {
-                    //if (matrix[h][w + 1] == 1) {
-                    adjacentList.add((matrixWidth * h) + (w + 1));
-                    //}
+                    if (matrix[h][w + 1] == 1) {
+                        adjacentList.add((matrixWidth * h) + (w + 1));
+                    }
                 }
-
-                //System.out.println("seen idx: " + currentElementIdx + " (value: " + nodeValues[currentElementIdx] + ")" + " connected to: " + adjacentList);
             }
         }
     }
 
     public List<Integer> riverSizes() {
 
+        List<Integer> rv = new LinkedList<>();
         boolean[] visited = new boolean[totalElementsOfMatrix];
-        Stack<Integer> stack = new Stack<>();
-        stack.add(0);
 
-        boolean isInRiver = false;
-        while (!stack.isEmpty()) {
-            int thisNodeIdx = stack.pop();
-            if (visited[thisNodeIdx]) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (Integer riverNode : riverNodes) {
+
+            if (visited[riverNode]) {
                 continue;
             }
 
-            System.out.println("Seen: [" + nodeValues[thisNodeIdx] + "] ");
-            visited[thisNodeIdx] = true;
-            List<Integer> adjacents = adjacentLists[thisNodeIdx];
-            boolean adjacentsInRiver = false;
-            for (int i = 0; i < adjacents.size(); i++) {
-                int adjacentIdx = adjacents.get(i);
-                if (nodeValues[adjacentIdx] == 1) {
-                    adjacentsInRiver = true;
-                    break;
+            stack.add(riverNode);
+
+            int riverSize = 0;
+
+            while (!stack.isEmpty()) {
+                int thisNodeIdx = stack.pop();
+                if (visited[thisNodeIdx]) {
+                    continue;
                 }
-            }
 
-            if (isInRiver != adjacentsInRiver) {
-                System.out.println("River breaks: " + thisNodeIdx);
+                System.out.println("seen: " + thisNodeIdx);
+                riverSize++;
+                visited[thisNodeIdx] = true;
+                List<Integer> adjacents = adjacentLists[thisNodeIdx];
+                stack.addAll(adjacents);
             }
-
-            isInRiver = adjacentsInRiver;
-            stack.addAll(adjacents);
+            rv.add(riverSize);
+            System.out.println("riverSize: " + riverSize);
         }
 
-        return Arrays.asList(1);
+        return rv;
     }
 }
--- algoexpert/src/test/java/com/zalizniak/medium/river_sizes/RiverSizesTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/river_sizes/RiverSizesTest.java
@@ -5,17 +5,21 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
+import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
+import static org.junit.Assert.assertThat;
+
 public class RiverSizesTest {
 
     @Test
     public void test() {
-        //Assert.assertEquals(Arrays.asList(1), new RiverSizes(new int[][]{{1}}).riverSizes());
-        Assert.assertEquals(Arrays.asList(1, 2, 2, 2, 5), new RiverSizes(new int[][]{
+
+        Assert.assertEquals(Arrays.asList(1), new RiverSizes(new int[][]{{1}}).riverSizes());
+        assertThat(new RiverSizes(new int[][]{
                 {1, 0, 0, 1, 0},
                 {1, 0, 1, 0, 0},
                 {0, 0, 1, 0, 1},
                 {1, 0, 1, 0, 1},
                 {1, 0, 1, 1, 0}
-        }).riverSizes());
+        }).riverSizes(), containsInAnyOrder(1, 2, 2, 2, 5));
     }
 }
