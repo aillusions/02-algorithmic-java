7edf4797e45fded4d9cd5f4e20ab69f101c4237a
alex <alex@platin.io>
2019-08-12 14:52:34 +0300

    River sizes

--- algoexpert/src/main/java/com/zalizniak/medium/river_sizes/RiverSizes.java
+++ algoexpert/src/main/java/com/zalizniak/medium/river_sizes/RiverSizes.java
@@ -10,6 +10,7 @@ import java.util.List;
 public class RiverSizes {
 
     private final List<Integer>[] adjacentLists;
+    private final Integer[] nodeValues;
     private final int totalElementsOfMatrix;
     private final int matrixWidth;
     private final int matrixHeight;
@@ -20,6 +21,7 @@ public class RiverSizes {
         matrixWidth = matrix[0].length;
         totalElementsOfMatrix = matrixWidth * matrixHeight;
         adjacentLists = new List[totalElementsOfMatrix];
+        nodeValues = new Integer[totalElementsOfMatrix];
 
         for (int h = 0; h < matrix.length; h++) {
             for (int w = 0; w < matrix.length; w++) {
@@ -27,42 +29,40 @@ public class RiverSizes {
                 int currentElementIdx = (matrixWidth * h) + w;
                 List<Integer> adjacentList = new LinkedList<>();
                 adjacentLists[currentElementIdx] = adjacentList;
-
-                int matrixElement = matrix[h][w];
-                if (matrixElement == 0) {
-                    continue;
-                }
+                nodeValues[currentElementIdx] = matrix[h][w];
 
                 if (h - 1 >= 0) {
-                    if (matrix[h - 1][w] == 1) {
-                        adjacentList.add((matrixWidth * (h - 1)) + w);
-                    }
+                    //if (matrix[h - 1][w] == 1) {
+                    adjacentList.add((matrixWidth * (h - 1)) + w);
+                    //}
                 }
 
                 if (h + 1 < matrixHeight) {
-                    if (matrix[h + 1][w] == 1) {
-                        adjacentList.add((matrixWidth * (h + 1)) + w);
-                    }
+                    //if (matrix[h + 1][w] == 1) {
+                    adjacentList.add((matrixWidth * (h + 1)) + w);
+                    //}
                 }
 
                 if (w - 1 >= 0) {
-                    if (matrix[h][w - 1] == 1) {
-                        adjacentList.add((matrixWidth * h) + (w - 1));
-                    }
+                    //if (matrix[h][w - 1] == 1) {
+                    adjacentList.add((matrixWidth * h) + (w - 1));
+                    //}
                 }
 
                 if (w + 1 < matrixWidth) {
-                    if (matrix[h][w + 1] == 1) {
-                        adjacentList.add((matrixWidth * h) + (w + 1));
-                    }
+                    //if (matrix[h][w + 1] == 1) {
+                    adjacentList.add((matrixWidth * h) + (w + 1));
+                    //}
                 }
 
-                System.out.println("seen: " + currentElementIdx + " connected to: " + adjacentList);
+                System.out.println("seen idx: " + currentElementIdx + " (value: " + nodeValues[currentElementIdx] + ")" + " connected to: " + adjacentList);
             }
         }
     }
 
     public List<Integer> riverSizes() {
+
+
         return Arrays.asList(1);
     }
 }
