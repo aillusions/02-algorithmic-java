186871742b439a733969155573094a2107d91ee5
alex <alex@platin.io>
2019-08-12 11:42:33 +0300

    BFS - shortest path

--- algoexpert/src/main/java/com/zalizniak/medium/graph/geeksforgeeks/ShortestPath.java
+++ algoexpert/src/main/java/com/zalizniak/medium/graph/geeksforgeeks/ShortestPath.java
@@ -5,13 +5,14 @@ package com.zalizniak.medium.graph.geeksforgeeks;
  */
 public class ShortestPath {
 
-    // directed unweighted predefined graph of 5 nodes, see
+    // directed unweighted predefined graph of 6 nodes, see
     private int[][] adjacencyLists = new int[][]{
-            /*0*/ {1, 2},
-            /*1*/ {2, 3, 4},
-            /*2*/ {3},
-            /*3*/ {4},
-            /*4*/ {0}
+            /*0*/ {1, 2, 5},
+            /*1*/ {4},
+            /*2*/ {},
+            /*3*/ {1, 5},
+            /*4*/ {0, 3},
+            /*5*/ {2}
     };
 
 }
