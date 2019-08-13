b776d0957b96bd1b80df9d42920c9facec645c16
alex <alex@platin.io>
2019-08-13 16:54:13 +0300

    Min max stack construction - wip

---
+++ algoexpert/src/main/java/com/zalizniak/medium/stack/MinStack.java
@@ -0,0 +1,36 @@
+package com.zalizniak.medium.stack;
+
+class MinStack {
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return -1;
+    }
+
+    public int getMin() {
+        return -1;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
---
+++ algoexpert/src/test/java/com/zalizniak/medium/stack/MinStackTest.java
@@ -0,0 +1,20 @@
+package com.zalizniak.medium.stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinStackTest {
+
+    @Test
+    public void test() {
+        MinStack stack = new MinStack();
+
+        stack.push(10);
+        stack.push(1);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+
+        Assert.assertEquals(1, stack.getMin());
+    }
+}
