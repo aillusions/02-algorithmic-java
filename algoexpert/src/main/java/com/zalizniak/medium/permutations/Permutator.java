1dee9a9aa54ec39e83b5c6fb0912436af127cb46
alex <alex@platin.io>
2019-08-13 10:41:00 +0300

    Permutations - wip

---
+++ algoexpert/src/main/java/com/zalizniak/medium/permutations/Permutator.java
@@ -0,0 +1,8 @@
+package com.zalizniak.medium.permutations;
+
+public class Permutator {
+
+    public static int[][] permutate(int[] in) {
+        return null;
+    }
+}
---
+++ algoexpert/src/test/java/com/zalizniak/medium/permutations/PermutatorTest.java
@@ -0,0 +1,11 @@
+package com.zalizniak.medium.permutations;
+
+import org.junit.Test;
+
+public class PermutatorTest {
+
+    @Test
+    public void test() {
+        Permutator.permutate(new int[]{1, 2, 3});
+    }
+}
