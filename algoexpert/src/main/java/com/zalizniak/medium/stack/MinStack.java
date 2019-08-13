94d033759f7ed766218fb040785c273ba4d9219a
alex <alex@platin.io>
2019-08-13 17:42:23 +0300

    Min max stack construction - wip

--- algoexpert/src/main/java/com/zalizniak/medium/stack/MinStack.java
+++ algoexpert/src/main/java/com/zalizniak/medium/stack/MinStack.java
@@ -1,36 +1,38 @@
 package com.zalizniak.medium.stack;
 
+import java.util.Stack;
+
 class MinStack {
 
-    /**
-     * initialize your data structure here.
-     */
-    public MinStack() {
-
-    }
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> min = new Stack<>();
 
     public void push(int x) {
 
+        if (stack.isEmpty()) {
+            stack.push(x);
+            min.push(x);
+        } else {
+            stack.push(x);
+            int y = min.peek();
+            if (x < y) {
+                min.push(x);
+            } else {
+                min.push(y);
+            }
+        }
     }
 
     public void pop() {
-
+        stack.pop();
+        min.pop();
     }
 
     public int top() {
-        return -1;
+        return stack.peek();
     }
 
     public int getMin() {
-        return -1;
+        return min.peek();
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(x);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
--- algoexpert/src/test/java/com/zalizniak/medium/stack/MinStackTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/stack/MinStackTest.java
@@ -10,11 +10,22 @@ public class MinStackTest {
         MinStack stack = new MinStack();
 
         stack.push(10);
-        stack.push(1);
         stack.push(20);
+        stack.push(2);
         stack.push(30);
+        stack.push(1);
         stack.push(40);
 
         Assert.assertEquals(1, stack.getMin());
+
+        stack.pop();
+        stack.pop();
+
+        Assert.assertEquals(2, stack.getMin());
+
+        stack.pop();
+        stack.pop();
+
+        Assert.assertEquals(10, stack.getMin());
     }
 }
