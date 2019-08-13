283c29be60f765d59ed9f20baad861d0dba8b480
alex <alex@platin.io>
2019-08-13 11:07:57 +0300

    Permutations - wip

--- algoexpert/src/main/java/com/zalizniak/medium/permutations/Permutator.java
+++ algoexpert/src/main/java/com/zalizniak/medium/permutations/Permutator.java
@@ -1,8 +1,31 @@
 package com.zalizniak.medium.permutations;
 
+/**
+ * The number of possible permutations grows factorially
+ * By the time you have 10 elements, there are more than 3.5 million permutations
+ * With factorial algorithms - computer is unable to compute more than the first dozen or so cases in any reasonable amount of time.
+ */
 public class Permutator {
 
     public static int[][] permutate(int[] in) {
-        return null;
+        int size = in.length;
+        int permutationsNumber = factorialUsingForLoop(size);
+        int[][] rv = new int[permutationsNumber][size];
+        return rv;
+    }
+
+    public static int factorialUsingForLoop(int n) {
+        int fact = 1;
+        for (int i = 2; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+
+    public static int factorialUsingRecursion(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        return n * factorialUsingRecursion(n - 1);
     }
 }
--- algoexpert/src/test/java/com/zalizniak/medium/permutations/PermutatorTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/permutations/PermutatorTest.java
@@ -2,10 +2,39 @@ package com.zalizniak.medium.permutations;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class PermutatorTest {
 
     @Test
-    public void test() {
-        Permutator.permutate(new int[]{1, 2, 3});
+    public void test1() {
+        int[][] permutations = Permutator.permutate(new int[]{1});
+        for (int i = 0; i < permutations.length; i++) {
+            System.out.println(Arrays.toString(permutations[i]));
+        }
+    }
+
+    @Test
+    public void test2() {
+        int[][] permutations = Permutator.permutate(new int[]{1, 2});
+        for (int i = 0; i < permutations.length; i++) {
+            System.out.println(Arrays.toString(permutations[i]));
+        }
+    }
+
+    @Test
+    public void test3() {
+        int[][] permutations = Permutator.permutate(new int[]{1, 2, 3});
+        for (int i = 0; i < permutations.length; i++) {
+            System.out.println(Arrays.toString(permutations[i]));
+        }
+    }
+
+    @Test
+    public void test4() {
+        int[][] permutations = Permutator.permutate(new int[]{1, 2, 3, 4});
+        for (int i = 0; i < permutations.length; i++) {
+            System.out.println(Arrays.toString(permutations[i]));
+        }
     }
 }
