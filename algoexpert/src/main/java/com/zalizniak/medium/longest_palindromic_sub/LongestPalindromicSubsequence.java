848c1018827d9fce40843e2a8d854e29371ea913
alex <alex@platin.io>
2019-08-14 12:38:13 +0300

    Longest Palindromic sub-sequence/string - wip

--- algoexpert/src/main/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubsequence.java
+++ algoexpert/src/main/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubsequence.java
@@ -1,8 +1,38 @@
 package com.zalizniak.medium.longest_palindromic_sub;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestPalindromicSubsequence {
 
+    private Map<String, Integer> cache = new HashMap<>();
+
     public int longestPalindromeSubseq(String s) {
-        return 0;
+
+        if (cache.containsKey(s)) {
+            return cache.get(s);
+        }
+
+        int rv;
+        System.out.println("Checking: " + s);
+        if (s.length() <= 1) {
+            rv = 0;
+        } else if (PalindromeChecker.isPalindrome(s)) {
+            rv = s.length();
+        } else {
+            rv = Integer.MIN_VALUE;
+            for (int i = 0; i < s.length(); i++) {
+                String sub = removeChar(s, i);
+                rv = Math.max(rv, longestPalindromeSubseq(sub));
+            }
+        }
+
+        cache.put(s, rv);
+
+        return rv;
+    }
+
+    public static String removeChar(String s, int index) {
+        return s.substring(0, index) + s.substring(index + 1);
     }
 }
