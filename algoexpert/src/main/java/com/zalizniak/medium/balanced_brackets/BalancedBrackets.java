02a1c18513c9030433d581b45796b1f4596be2ec
alex <alex@platin.io>
2019-08-14 10:37:12 +0300

    Balanced brackets - wip

--- algoexpert/src/main/java/com/zalizniak/medium/balanced_brackets/BalancedBrackets.java
+++ algoexpert/src/main/java/com/zalizniak/medium/balanced_brackets/BalancedBrackets.java
@@ -1,8 +1,29 @@
 package com.zalizniak.medium.balanced_brackets;
 
+import java.util.Stack;
+
 public class BalancedBrackets {
 
+    private static final char OPEN = '[';
+    private static final char CLOSE = ']';
+
     public static boolean isBalanced(String in) {
-        return false;
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < in.length(); i++) {
+            char charAt = in.charAt(i);
+            boolean open = (charAt == OPEN);
+            if (open) {
+                stack.push(1);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
     }
 }
--- algoexpert/src/test/java/com/zalizniak/medium/balanced_brackets/BalancedBracketsTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/balanced_brackets/BalancedBracketsTest.java
@@ -6,13 +6,38 @@ import org.junit.Test;
 public class BalancedBracketsTest {
 
     @Test
-    public void test() {
+    public void testClear() {
         Assert.assertTrue(BalancedBrackets.isBalanced("[]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[][][]"));
         Assert.assertTrue(BalancedBrackets.isBalanced("[[[]]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[][][]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[][[][]][]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[]]"));
 
         Assert.assertFalse(BalancedBrackets.isBalanced("["));
+        Assert.assertFalse(BalancedBrackets.isBalanced("][]["));
+        Assert.assertFalse(BalancedBrackets.isBalanced("]["));
         Assert.assertFalse(BalancedBrackets.isBalanced("[]]"));
         Assert.assertFalse(BalancedBrackets.isBalanced("]]"));
+        Assert.assertFalse(BalancedBrackets.isBalanced("[[[]]"));
         Assert.assertFalse(BalancedBrackets.isBalanced("]][[]]"));
+    }
+
+    @Test
+    public void testNoisy() {
+        Assert.assertTrue(BalancedBrackets.isBalanced("[1]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[a][b][c]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[[try]]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[n][i][j]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("n[[p][[h][f]]a[]]"));
+        Assert.assertTrue(BalancedBrackets.isBalanced("[[]]"));
+
+        Assert.assertFalse(BalancedBrackets.isBalanced("r["));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r][]["));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r]["));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r[]]"));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r]]"));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r[[[]]"));
+        Assert.assertFalse(BalancedBrackets.isBalanced("r]][[]]"));
     }
 }
