9a4b5426bdaf59332a9b9bea15582f885ff9827b
alex <alex@platin.io>
2019-08-14 11:27:04 +0300

    Longest Palindromic sub-sequence/string - wip

--- algoexpert/src/main/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubstring.java
+++ algoexpert/src/main/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubstring.java
@@ -3,6 +3,15 @@ package com.zalizniak.medium.longest_palindromic_sub;
 public class LongestPalindromicSubstring {
 
     public int longestPalindromeSubstr(String s) {
+
+        if (s.length() <= 1) {
+            return 0;
+        }
+
+        if (PalindromeChecker.isPalindrome(s)) {
+            System.out.println("Found palindrome with length: " + s.length() + " ( " + s + " )");
+        }
+
         return 0;
     }
 }
--- algoexpert/src/test/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubstringTest.java
+++ algoexpert/src/test/java/com/zalizniak/medium/longest_palindromic_sub/LongestPalindromicSubstringTest.java
@@ -9,11 +9,12 @@ public class LongestPalindromicSubstringTest {
 
     @Test
     public void test() {
+        Assert.assertEquals(3, gen.longestPalindromeSubstr("RTR")); // RTR
         Assert.assertEquals(3, gen.longestPalindromeSubstr("abxyx")); // xyx
         Assert.assertEquals(3, gen.longestPalindromeSubstr("BBABCBCAB")); // BAB, CBC
         Assert.assertEquals(3, gen.longestPalindromeSubstr("bbbab")); // bbb
         Assert.assertEquals(2, gen.longestPalindromeSubstr("cbbd")); // bb
-        Assert.assertEquals(6, gen.longestPalindromeSubstr("1abba1")); // 1abba1
-        Assert.assertEquals(4, gen.longestPalindromeSubstr("1abba2")); // abba
+        Assert.assertEquals(6, gen.longestPalindromeSubstr("viv1abba1")); // 1abba1
+        Assert.assertEquals(4, gen.longestPalindromeSubstr("1abba2TiT")); // abba
     }
 }
