5a3b76e23b4f3b7cef580aecc486a1f20742629d
alex <alex@platin.io>
2019-08-31 22:31:39 +0300

    Semi primes

--- algoexpert/src/test/java/com/zalizniak/crypto/PrimeNumbers.java
+++ algoexpert/src/test/java/com/zalizniak/crypto/PrimeNumbers.java
@@ -66,6 +66,14 @@ public class PrimeNumbers {
         System.out.println(primeFactors(32));
     }
 
+    // 4, 6, 9, 10, 14, 15, 21, 22, 25, 26, 33, 34, 35, 38, 39, 46, 49, 51, 55, 57, 58, 62, 65, 69, 74, 77, 82, 85, 86, 87, 91, 93, 94, 95
+    @Test
+    public void testSemiprime() {
+        System.out.println(primeFactors(57));
+        System.out.println(primeFactors(86));
+        System.out.println(primeFactors(95));
+    }
+
     // 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
     public boolean isPrime(long n) {
 
