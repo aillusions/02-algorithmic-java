e989369d65694f7ec29e21b9f54fec68c0711716
alex <alex@zalizniak.com>
2019-09-03 15:43:00 +0300

    bitwise

--- algoexpert/src/test/java/com/zalizniak/bitwise/BitwiseTest.java
+++ algoexpert/src/test/java/com/zalizniak/bitwise/BitwiseTest.java
@@ -15,29 +15,29 @@ public class BitwiseTest {
 
     @Test
     public void intToBytes() {
-        for (byte b : intToBytes(1_695_609_641)) {
+        for (byte b : intToBytes(1)) {
             System.out.format("0x%x ", b);
         }
 
         System.out.println();
 
-        for (byte b : intToByteArray(1_695_609_641)) {
+        for (byte b : intToByteArray(1)) {
             System.out.format("0x%x ", b);
         }
 
         System.out.println();
     }
 
-    public static final byte[] intToBytes(int value) {
-        byte[] bytes = ByteBuffer.allocate(4).putInt(1_695_609_641).array();
-
-       return bytes;
+    public static byte[] intToBytes(int value) {
+        return ByteBuffer.allocate(4).putInt(value).array();
     }
-    public static final byte[] intToByteArray(int value) {
-        return new byte[] {
-                (byte)(value >>> 24),
-                (byte)(value >>> 16),
-                (byte)(value >>> 8),
-                (byte)value};
+
+    public static byte[] intToByteArray(int value) {
+        return new byte[]{
+                (byte) (value >>> 24),
+                (byte) (value >>> 16),
+                (byte) (value >>> 8),
+                (byte) value
+        };
     }
 }
