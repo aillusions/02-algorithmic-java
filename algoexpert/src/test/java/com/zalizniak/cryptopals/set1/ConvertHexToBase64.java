a5c570e3f9cf84ec02d1c86a7d89a0cd2ffc80c6
alex <alex@zalizniak.com>
2019-09-03 15:30:35 +0300

    Hex to base64 - more manual solution

--- algoexpert/src/test/java/com/zalizniak/cryptopals/set1/ConvertHexToBase64.java
+++ algoexpert/src/test/java/com/zalizniak/cryptopals/set1/ConvertHexToBase64.java
@@ -34,14 +34,75 @@ import java.util.Base64;
  */
 public class ConvertHexToBase64 {
 
+    public final static char[] base64alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".toCharArray();
+
     @Test
     public void test() {
         final String base16 = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
         final String base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+
         Assert.assertEquals(base64, hexToBase64(base16));
+        Assert.assertEquals(base64, base16to64(base16));
     }
 
     public static String hexToBase64(String in) {
         return Base64.getEncoder().encodeToString(new BigInteger(in, 16).toByteArray());
+    }
+
+    public static String base16to64(String hex) {
+        BigInteger value = base16to10(hex); //base 10 value
+
+        // --- convert base 10 to 64 ---
+        int digits = 1; // # digits in base 64 we'll need
+        while (value.compareTo(new BigInteger("64").pow(digits)) != -1) { //while the decimal value >= 64^i
+            digits++;
+        }
+
+        String ret = "";
+        for (int i = digits - 1; i >= 0; i--) { //start at most significant digit
+
+            //this digit must be an A (represents 0)
+            if (value.subtract(new BigInteger("64").pow(i)).compareTo(BigInteger.ZERO) == -1) {
+                ret += "A";
+            } else {
+                for (int d = 63; d >= 1; d--) {
+                    //digit found: [ value - (64^i)*d >= 0 ]  with d the greatest possible
+                    if (value.subtract(new BigInteger("64").pow(i).multiply(new BigInteger(Integer.toString(d)))).compareTo(BigInteger.ZERO) != -1) {
+                        value = value.subtract(new BigInteger("64").pow(i).multiply(new BigInteger(Integer.toString(d))));
+                        ret += base64alphabet[d];
+                        break;
+                    }
+                }
+            }
+        }
+
+        return ret;
+    }
+
+    /**
+     * Return the BigInteger decimal value of an input hexadecimal string.
+     */
+    public static BigInteger base16to10(String hexString) {
+        hexString = hexString.toLowerCase();
+
+        //hexString is 0-9 then a-f
+        BigInteger value = BigInteger.ZERO; //base 10 value
+        for (int i = 0; i < hexString.length(); i++) { //moving from most significant digit towards 16^0
+
+            int digit = (int) hexString.charAt(i);
+            //if the hexString digit is 0-9
+            if ('0' <= hexString.charAt(i) && hexString.charAt(i) <= '9') {
+                digit = digit - '0';
+                //else the hexString digit is a-f
+            } else {
+                digit = digit - 'a' + 10;
+            }
+
+            BigInteger temp = new BigInteger("" + digit); // digit as BigInt
+            temp = temp.multiply(new BigInteger("16").pow(hexString.length() - 1 - i)); // digit * 16^index
+            value = value.add(temp);
+        }
+
+        return value;
     }
 }
