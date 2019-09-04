3811abd285f42c704c316d359f31a3b989882990
alex <alex@zalizniak.com>
2019-09-04 12:25:50 +0300

    percentages ..

---
+++ algoexpert/src/test/java/com/zalizniak/percent/Percents.java
@@ -0,0 +1,38 @@
+package com.zalizniak.percent;
+
+public class Percents {
+
+    // 10 is 20% of full amount.
+    // What is full amount?
+    // 1 / 0.20 * 10 == 50
+
+    // John is a quarterback. This year, he completed 350 passes, which is 70% of all the passes he's attempted this year.
+    // How many passes has John attempted this year?
+    // 1 / 0.70 * 350 = 500
+
+    // Emily's family loves to work together in the garden.
+    // They have a slight preference for flowers, as 60% of their plants are flowers and 40% are vegetables. They have 50 plants growing in the garden.
+    // How many vegetable plants do they have?
+    // 0.4 * 50 = 20
+
+    // In our solar system, 6 of the 8 planets have moons.
+    // What percentage of the planets have moons?
+    // 6 / 8 * 100 = 75%
+
+    // Mrs. Conley asks her class what kind of party they want to have to celebrate their excellent behavior.
+    // Out of all the students in the class, 5 want an ice cream party, 7 want a movie party, 10 want a costume party, and the rest are undecided.
+    // If 20% want an ice cream party, how many students are in the class?
+    // 1 / 0.20 * 5 == 25
+
+    // Elmer has a collection of 300 fossils. Of these, 21% are fossilized snail shells.
+    // How many fossilized snail shells does Elmer have?
+    // 300 * 0.21 == 63
+
+    // There are 25 students in Ms. Nguyen's second-grade class. In the class election, 4 students voted for Benjamin, 12 voted for Sahil, and 9 voted for Maria.
+    // What percentage of the class voted for Maria?
+    // 9 / 25 * 100 = 36
+
+    // What is the percent change from 2100 to 1890?
+    // 2100 - 1890 / 2100  * 100 = 10 %
+
+}
