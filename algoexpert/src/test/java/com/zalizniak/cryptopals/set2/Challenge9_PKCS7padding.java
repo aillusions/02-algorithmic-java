31015138b526b7f0323f9fc93b16830f35a2cc0e
alex <alex@zalizniak.com>
2019-09-05 21:35:14 +0300

    Challenge 9 - done

--- algoexpert/src/test/java/com/zalizniak/cryptopals/set2/Challenge9_PKCS7padding.java
+++ algoexpert/src/test/java/com/zalizniak/cryptopals/set2/Challenge9_PKCS7padding.java
@@ -17,6 +17,15 @@ public class Challenge9_PKCS7padding {
         Assert.assertArrayEquals(new byte[]{89, 69, 76, 76, 79, 87, 32, 83, 85, 66, 77, 65, 82, 73, 78, 69, 4, 4, 4, 4}, padded);
     }
 
+    @Test
+    public void test2() {
+        String origString = "YELLOW SUBMARINE";
+        byte[] orig = origString.getBytes(StandardCharsets.UTF_8);
+        byte[] padded = padBlock(orig, 19);
+        System.out.println(Arrays.toString(orig) + " -> " + Arrays.toString(padded));
+        Assert.assertArrayEquals(new byte[]{89, 69, 76, 76, 79, 87, 32, 83, 85, 66, 77, 65, 82, 73, 78, 69, 3, 3, 3}, padded);
+    }
+
     /**
      * The value of each added byte is the number of bytes that are added, i.e. N bytes, each of value N are added.
      * The number of bytes added will depend on the block boundary to which the message needs to be extended.
