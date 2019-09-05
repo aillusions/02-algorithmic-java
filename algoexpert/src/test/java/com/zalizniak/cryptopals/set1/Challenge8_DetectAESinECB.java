25dea07fdc01f093c758893cf098ab0f810b003b
alex <alex@zalizniak.com>
2019-09-05 19:54:44 +0300

    Challenge 8 - wip

--- algoexpert/src/test/java/com/zalizniak/cryptopals/set1/Challenge8_DetectAESinECB.java
+++ algoexpert/src/test/java/com/zalizniak/cryptopals/set1/Challenge8_DetectAESinECB.java
@@ -1,26 +1,40 @@
 package com.zalizniak.cryptopals.set1;
 
 import com.zalizniak.ByteArraysTest;
+import com.zalizniak.HexTest;
 import org.junit.Test;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 /**
  * https://cryptopals.com/sets/1/challenges/8
  */
 public class Challenge8_DetectAESinECB {
 
+    public static int ECB_BLOCK_SIZE = 16;
+
     @Test
     public void test1() {
-        for (String cipherText : BASE64_TEXT) {
-            byte[] bytes = cipherText.getBytes(StandardCharsets.UTF_8);
-            byte[][] matrix = Challenge6_BreakRepeatingKeyXOR.ciphertextBlocks(bytes, 16);
+        for (String cipherTextHex : BASE64_TEXT) {
+            String cipherText = HexTest.hexToASCII(cipherTextHex);
+            byte[] cipherTextBytes = cipherText.getBytes(StandardCharsets.UTF_8);
+            byte[][] cipherTextBytesMatrix = Challenge6_BreakRepeatingKeyXOR.ciphertextBlocks(cipherTextBytes, ECB_BLOCK_SIZE);
 
-            System.out.println(ByteArraysTest.printGrid(matrix));
-            System.out.println();
+            for (int i = 0; i < cipherTextBytesMatrix.length; i++) {
+                for (int j = i + 1; j < cipherTextBytesMatrix.length; j++) {
+                    if (Arrays.equals(cipherTextBytesMatrix[i], cipherTextBytesMatrix[j])) {
+                        System.out.println("cipherTextHex: " + cipherTextHex);
+                        System.out.println("Found equal: \n" + Arrays.toString(cipherTextBytesMatrix[i]) + " \n" + Arrays.toString(cipherTextBytesMatrix[j]));
+
+                        System.out.println();
+                        System.out.println(ByteArraysTest.printGrid(cipherTextBytesMatrix));
+                        System.out.println();
+                    }
+                }
+            }
         }
     }
-
 
     public static final String[] BASE64_TEXT = {
             "8a10247f90d0a05538888ad6205882196f5f6d05c21ec8dca0cb0be02c3f8b09e382963f443aa514daa501257b09a36bf8c4c392d8ca1bf4395f0d5f2542148c7e5ff22237969874bf66cb85357ef99956accf13ba1af36ca7a91a50533c4d89b7353f908c5a166774293b0bf6247391df69c87dacc4125a99ec417221b58170e633381e3847c6b1c28dda2913c011e13fc4406f8fe73bbf78e803e1d995ce4d",
@@ -155,7 +169,7 @@ public class Challenge8_DetectAESinECB {
             "143c6e7b667e6c8872108da70e4896accabea7b76163b3c4ee3bf0f3551a24d86aa4ae029579e21e23ca5504658721a0198d79b64270e9f248d588fb6c5a7f6f4f618fba10b03b4d687aba7d4464526d27abc7ae7d3f3669340f1af280ab5efdef1364450cc076e3e046c2fb532edcde528a4b7d393ff33b2bc530066d208d8d7a0229012a1f0f310f3f2345ff84fc68ea8734d26718626a06b029b5b2762c09",
             "df37782dc76e95d8c84cb883c5517025b42a04de301aef0656e1bf09fa152ea9382d8218ab4dc7d23f50542559b8d2242fed01bc44e560a75f6d2bd3783d87e3230206d4c5f0e046d4da193807f4a53eb0f7234ad4e6c3246ae8f848d7af73a6976f2fb9dbf3d4f190c2e94bb8d98127df13ec6b30a6781bd36a9a90c0e1f9a09231f309ad2a89f94cbbf45f9e0fe34191faf95dd1482a6fea01fb1ed677bc4c",
             "5c4ca78f8de3527788e7d1efcd6aad0adc3878ea70993ae20937ef0a601730494946f078de2099c62de9af1c47ee4f18216ed5a7268464f210374dbf421d55449c8f399d8824c5a0ff8526a940223ee999a6f945f0ba3eaa672c434ad867ac7adaa46bd3289729c6c7d920dd0d8237bf678d88bde91e0683e72e88fef50bdb23cceb6270acba5aeebd0a834ccf99cd3e6bad8c158f5819f1f1c785fdaa3df505",
-            "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a",
+            "d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a", // This one?
             "b563aac8275730bd4cf89ab32bb4b152be8fae16afab58ab3ea0e825c8ce28ddbe26c8cafef763f1d9c3f30d60335cd0b765b98a11d5cfbe7a2d75e8f8a5e851ee6a17de174d8bea5c1e089beffc99709d6dcc03e578220eccdfa99d3fa0a3d2f6736de041cd783ad7f866df5dcd2a752cfbfc380cf84da5c5dd3fc486cf1adc14d29d9e91737514e8c67d5c5aece4a19216e2b069f53b8ab4acaef17f815004",
             "61750df604b4ddc333b9e669b218e46bbe50be4b2d3a76e8043e79e98eac204797138e0eae7f39ce256a1a315476039a8f0776a724270f32b32c973d6316f281081970dccb8c13b9f8df927b1dd29eec37f4b68a3e3179a62329bcee1407d00cb1ff2a636a42ca8e939e6a9c56a382289ae5cbb7eb07df1182a7ef10882f28a7fe5d8582a2fb93b4fdf02ccd5b9050b60665771a21c0f35cb29eea40710dec75",
             "32d13a6e522e33e556b5b084b1e6811157e88ef0a897b6e62d31c984a51957eebf6b1e46ea80b1def31d34a99bb5a630d9127537537dda3f88eaf2bee4c37d859cf91387963e67748e7c09b3e0c1c8ce56937bded53e2b353dcb39e8aafe8ccdf71e82d88df62184750b125bd09064e296c5caca1122ec13aa030cf79ab9394746a377ae2e93a78986fa3531098c3e904a6b0cddaa4c8dea9b807cf63f464dce",
