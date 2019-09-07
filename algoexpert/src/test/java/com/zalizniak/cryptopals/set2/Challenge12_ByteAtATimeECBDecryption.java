b64584c4c2a5f1beb029470eb9af19b38e5e58cc
alex <alex@platin.io>
2019-09-07 21:27:26 +0300

    Challenge 12 - wip

---
+++ algoexpert/src/test/java/com/zalizniak/cryptopals/set2/Challenge12_ByteAtATimeECBDecryption.java
@@ -0,0 +1,16 @@
+package com.zalizniak.cryptopals.set2;
+
+import org.junit.Test;
+
+/**
+ * https://cryptopals.com/sets/2/challenges/12
+ * <p>
+ * Byte-at-a-time ECB decryption (Simple)
+ */
+public class Challenge12_ByteAtATimeECBDecryption {
+
+    @Test
+    public void testRandomByteArray() {
+
+    }
+}
